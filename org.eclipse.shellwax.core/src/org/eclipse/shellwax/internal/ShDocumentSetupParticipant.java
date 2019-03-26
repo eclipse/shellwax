16f4f864c142efaba155beef9569eeef601936f4
Alexander Kurtakov <akurtako@redhat.com>
2019-03-27 00:20:43 +0200

    Ensure newly created files have proper line delimiters for shell scripts even on windows.
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

---
+++ org.eclipse.shellwax.core/src/org/eclipse/shellwax/internal/ShDocumentSetupParticipant.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Red Hat Inc. and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *  Alexander Kurtakov (Red Hat Inc.)- initial implementation
+ *******************************************************************************/
+package org.eclipse.shellwax.internal;
+
+import org.eclipse.core.filebuffers.IDocumentSetupParticipant;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IDocumentExtension4;
+
+/**
+ * Files of type sh should always have unix line delimiter. This ensures it for newly created files even on windows.
+ *
+ */
+public class ShDocumentSetupParticipant implements IDocumentSetupParticipant {
+
+	@Override
+	public void setup(IDocument document) {
+		if (document instanceof IDocumentExtension4) {
+			((IDocumentExtension4)document).setInitialLineDelimiter("\n");
+		}
+		
+	}
+
+}
